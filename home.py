83444ea4a44f9f558f9d952f6c2abb86b7049cd9
Priya <pj19510112@gmail.com>
2026-08-23 22:23:02 +0530

    Add emoji to page title in home.py

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ import streamlit as st
 # ==========================================================
 
 st.set_page_config(
-    page_title="Telco Churn Intelligence",
+    page_title="📡Telco Churn Intelligence",
     page_icon="📡",
     layout="wide",
     initial_sidebar_state="expanded"
